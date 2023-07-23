89631818e6114adf308a9d45b5d115008dff82c3
Gaston <gaston@tdil.com.ar>
2023-07-23 13:10:58 -0300

    se va a trabajar con la libreria Three.js directamente, desde el ngOnInit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,65 +1,73 @@
-import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ElementRef } from '@angular/core';
 import { BehaviorSubject, interval } from 'rxjs';
 import { AutenticacionService } from 'src/app/services/autenticacion.service';
+
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  template: `
+    <canvas #canvasElement></canvas>
+  `
 })
 export class HomeComponent implements OnInit {
-  openLogin: boolean = false;
-  closeLogin: boolean = false;
-  logeado: boolean = false;
+  @ViewChild('canvasElement', { static: true }) canvasElementRef!: ElementRef<HTMLCanvasElement>;
 
-   saludos = [
-    "Hola",
-    "Hello",
-    "こんにちは",
-    "Salut",
-    "你好",
-    "Ciao",
-    "안녕하세요"
-  ]
-
-  SALUDO = '';
-
-  constructor(private authServ : AutenticacionService) {
-
-  }
-
+  constructor() {}
 
   ngOnInit(): void {
-    const time$ = interval(2000);
-    time$.subscribe(() =>{
-      //funcion length saca cuanto elementos tiene y random usa eso.
-    let saludoRandom = Math.floor(Math.random()*this.saludos.length);
-      //segun la posicion traigo el saludo
-    let  nSaludo = this.saludos[saludoRandom];
-      //imprimo el saludo
-    this.SALUDO = nSaludo;
-      return this.SALUDO;
-    });
+    // Scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-    if (this.authServ.UsuarioAutenticado == true){
-      this.logeado = this.authServ.UsuarioAutenticado;
-      this.isLogeado();
+    const renderer = new THREE.WebGLRenderer(
+      {canvas: this.canvasElementRef.nativeElement}
+    );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    //reSizes fuction
+    window.addEventListener('resize', () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      camera.updateMatrixWorld();
+      renderer.setSize(window.innerWidth, window.innerHeight,)
+    })
+
+    //geometry
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    // Rendering the scene
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
     }
+    animate();
   }
+
+
+
+
   activeEstady(){
-    if (this.closeLogin == false) {
-      this.openLogin = !this.openLogin;
-    }
   }
   isLogeado(){
-    if(this.logeado == true){
-      this.closeLogin = !this.closeLogin;
-    }
    }
   disconect(){
-   // this.authServ.currentUserSubject = new BehaviorSubject<any>(JSON.parse(sessionStorage.getItem('currentUser')|| '{}'));
-   this.authServ.disconection();
-   this.ngOnInit();
   }
 
 }
